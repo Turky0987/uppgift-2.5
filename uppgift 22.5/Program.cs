5f338529a64fe2dbea697bf272b2fe47a2f6f9b4
Tareq Bilal Al-Asaad <taal04003@utb.vaxjo.se>
2023-09-22 13:22:24 +0200

    Add project files.

---
+++ uppgift 22.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        Console.Write("Enter a calculation: ");
+        string input = Console.ReadLine();
+
+        string[] parts = input.Split('*');
+
+        if (parts.Length == 2 && double.TryParse(parts[0], out double tal1) && double.TryParse(parts[1], out double tal2))
+        {
+            double resultat = tal1 * tal2;//
+        }
+    }
+}
